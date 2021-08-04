ba4fa51838395a60faba62c2a98b6b16ede3a5cb
Luiz Henrique <luizhjramos@outlook.com>
2021-08-04 19:07:50 -0300

    Menu da aplicação

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Nav } from './styled'
+
+export default function Header() {
+  return (
+    <Nav>
+      <a href="/">Home</a>
+      <a href="/">Sobre</a>
+    </Nav>
+  )
+}
