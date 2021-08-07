97dede2e679a0b7f8ba966927126427b23ce0201
Luiz Henrique <luizhjramos@outlook.com>
2021-08-07 14:13:26 -0300

    Configurando o axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,7 +3,19 @@ import React from 'react'
 import { Container } from '../../styles/GlobalStyles'
 import { Title } from './styled'
 
+import axios from '../../services/axios'
+
 export default function Login() {
+
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos')
+      console.log(response)
+    }
+
+    getData()
+  }, [])
+
   return (
     <Container>
       <Title isRed={false}>
